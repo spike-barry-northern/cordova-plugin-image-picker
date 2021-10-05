756f70ff8f51aef92aa9215a79963411b2434bed
Rob Tuvey <rob.tuvey@spikeglobal.com>
2021-10-05 10:39:42 +0100

    Added logging.

--- src/android/MainActivity.java
+++ src/android/MainActivity.java
@@ -1,6 +1,7 @@
 package spikeglobal.cordova;
 
 import org.apache.cordova.CordovaActivity;
+import android.util.Log;
 
 public class MainActivity extends CordovaActivity {
 
@@ -8,8 +9,12 @@ public class MainActivity extends CordovaActivity {
     public void init() {
         super.init();
 
+        Log.v("Image Picker", "Setting web view setting.");
+
         WebView webView = (SystemWebView)(appView.getEngine().getView());
         WebSettings webSettings = webView.getSettings();
         webSettings.setAllowFileAccess(true);
+
+        Log.i("Image Picker", "Set web view setting.");
     }
 }
